f89bb600b47edc062c88e9e89d60e2c6a5118491
rgr <rgr@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-01-31 16:43:59 +0000

    new
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@522 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dInterface/TTPanel.java
@@ -0,0 +1,19 @@
+package dInterface;
+
+/**
+ * <p>Title: miniDia</p>
+ * <p>Description: exam timetable construction with Condition Pattern</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: UdeS</p>
+ * @author rgr
+ * @version 1.0
+ */
+
+import javax.swing.JPanel;
+
+public class TTPanel extends JPanel {
+
+  public TTPanel() {
+    super();
+  }
+}
