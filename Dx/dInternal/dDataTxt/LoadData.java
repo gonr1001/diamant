0b9148b9d212284f6c6c48082f6d6ff38fec3ea9
rgr <rgr@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-02-20 19:56:23 +0000

    LoadData
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@526 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dInternal/dDataTxt/LoadData.java
@@ -0,0 +1,116 @@
+package dInternal;
+
+/**
+ * <p>Title: miniDia</p>
+ * <p>Description: exam timetable construction with Condition Pattern</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: UdeS</p>
+ * @author rgr
+ * @version 1.0
+ */
+
+import java.util.Vector;
+import java.util.StringTokenizer;
+
+import com.iLib.gDialog.FatalProblemDlg;
+import com.iLib.gIO.Filter;
+
+public class LoadData {
+  Vector _v;
+  String _instructorFileName;
+  String _roomsFileName;
+  String _activitiesFileName;
+  String _studentsFileName;
+
+  private final int NUMBER_OF_TOKENS = 4;
+  private final String CR_LF = "\r\n";
+  /***
+   *constructor
+   */
+  public LoadData(String args) {
+    _v = new Vector(); // to eliminate
+   verifyInportDataFile(args);
+  }
+
+  private void verifyInportDataFile(String str){
+    Filter filter = new Filter();
+    if (filter.doIt(str)) {
+      StringTokenizer st = new StringTokenizer(new String (filter.getByteArray()), CR_LF );
+      if (st.countTokens() == NUMBER_OF_TOKENS){
+        _instructorFileName = st.nextToken();
+        _roomsFileName = st.nextToken();
+        _activitiesFileName = st.nextToken();
+        _studentsFileName = st.nextToken();
+      } else {
+        new FatalProblemDlg(
+            "Wrong number of lines in the file:" +
+            str +
+            "\n" +
+            "I was in DLoadData constructor ");
+        System.exit(1);
+      }
+    } else {
+      /*new DisplayFatalProblem(
+          "Unable to filter a file" +
+          str +
+          "\n" +
+          "I was in DLoadData constructor ");*/ //ys
+      System.exit(1);
+    }
+  }
+  //args[0]: instructor
+  //args[1]: student
+  //args[2]:
+  //args[3]:
+  public void extractDatafromImport(String [] args){
+   // _v.add(extractInstructors(args[0]));
+   // _v.add(extractStudents(args[1]));
+  }
+
+  private Vector extractInstructors(){
+    byte[]  dataloaded = preLoad(_instructorFileName);
+    if (dataloaded != null) {
+      StringTokenizer st = new StringTokenizer(new String (dataloaded),"\r\n" );
+      return analyseInstructorTokens (st);
+    } else {// (NullPointerException npe) {
+      new FatalProblemDlg("npe.toString()" );
+      System.exit(52);
+    }
+    return null;
+  }
+
+
+  private byte[] preLoad(String str) {
+    Filter filter = new Filter();
+    filter.readFile(str);
+    if (filter.testValidityOfBytes()) {
+      filter.adjustingLines();
+      filter.adjustingEndFile();
+      return filter.getByteArray();
+    } else return null;
+
+  }
+
+  private Vector extractStudents(){
+    byte[]  dataloaded = preLoad(_studentsFileName);
+    if ( dataloaded!= null) {
+  StringTokenizer st = new StringTokenizer(new String(dataloaded),"\r\n" );
+  return analyseStudentTokens (st);
+} else {// (NullPointerException npe) {
+  new FatalProblemDlg("npe.toString()" );
+  System.exit(52);
+}
+return null;
+  }
+
+  private  Vector analyseInstructorTokens(StringTokenizer st) {
+    return new Vector();
+  }
+
+  private  Vector analyseStudentTokens(StringTokenizer st) {
+   return new Vector();
+  }
+
+
+
+}
