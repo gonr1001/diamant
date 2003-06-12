f63748a4a5de103271aa38e5e179eceb7a03ff78
alexj <alexj@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-06-12 10:22:08 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@693 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInterface/DToolBar.java
+++ Dx/dInterface/DToolBar.java
@@ -1,7 +1,7 @@
 package dInterface;
 
 /**
- * Title: ToolBar $Revision: 1.6 $  $Date: 2003-06-11 08:07:07 $
+ * Title: ToolBar $Revision: 1.7 $  $Date: 2003-06-12 10:22:08 $
  * Description: ToolBar is a class used to display a
  *               toolbar with buttons
  *
@@ -17,7 +17,7 @@ package dInterface;
  * you entered into with rgr-fdl.
  *
  * @version $Version$
- * @author  $Author: rgr $
+ * @author  $Author: alexj $
  * @since JDK1.3
  */
 
@@ -32,6 +32,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
+import javax.swing.JToolBar.Separator;
+
 
 import dInternal.dTimeTable.TTStructure;
 
@@ -44,269 +46,242 @@ import dResources.DConst;
  */
 public class DToolBar extends JToolBar {// implements ActionListener{
   private DApplication _dApplic;
-  private static final String _strArray [] = {"Jours", "Periods"};
-  JPanel _jp = new JPanel();
-  JComboBox setToolBar, daysList;
-  JButton addDay, removeDay, loPriority, noPriority, hiPriority, sameLine, sameColumn, doIt;
-  JTextField setDays, dayName, periodIndicator;
-  JLabel lDaysList, lSetDays, lDayName;
+  private static final String _toolBarNames [] = {"Jours", "Periods"};
+  JComboBox toolBarSelector, daySelector, dayNameSelector, periodSelector, periodTypeSelector;
+  JButton stdDays, addDay, removeDay, sameLine, sameColumn;
+  JTextField setNumberOfDays;
+  JLabel lSetNumberOfDays, lDaySelector, lDayNameSelector, lPeriodIndicator, lPeriodTypeSelector;
+  JToolBar.Separator jtbSep [];
+
 
 
   //-------------------------------------------
   public DToolBar(DApplication dApplic) {
     _dApplic = dApplic ;
-
-
     //The JLabel Objects initialisation
-    lSetDays = new JLabel("Nomber of Days ");
-    lDaysList = new JLabel("Choise day ");
-    lDayName = new JLabel("Name of day ");
+    lSetNumberOfDays = new JLabel("Nombre de jours ");
+    lDaySelector = new JLabel("Jour courrant ");
+    lDayNameSelector = new JLabel("Nom du jour ");
+    lPeriodIndicator = new JLabel("Index Période ");
+    lPeriodTypeSelector = new JLabel("Type Période ");
 
     //The JButton Objects initialisation
-    doIt = new JButton("Apply");
-    addDay = new JButton("Add day");
-    removeDay = new JButton("Remove day");
-    loPriority = new JButton("Low priority");
-    loPriority.setBackground(Color.cyan);
+    stdDays = new JButton("Standard");
+    addDay = new JButton("Ajouter");
+    removeDay = new JButton("Supprimer");
 
-    noPriority = new JButton("Normal priority");
-    noPriority.setBackground(Color.yellow);
-
-    hiPriority = new JButton("High priority");
-    hiPriority.setBackground(Color.red);
-    sameLine = new JButton("Apply to all line");
-    sameColumn = new JButton("Apply to all column");
+    sameLine = new JButton("Toute la journée");
+    sameColumn = new JButton("Toute la ligne");
 
 
-    //The textField objects initialisation
-    dayName = new JTextField();
-    dayName.setMaximumSize(new Dimension(50, DConst.NPT11 * 2));
+    //textField objects initialisation
+    setNumberOfDays = new JTextField();
+    setNumberOfDays.setMaximumSize(new Dimension(30, DConst.NPT11 * 2));
 
-    periodIndicator = new JTextField("Actual Period");
-    periodIndicator.setMaximumSize(new Dimension(100, DConst.NPT11 * 2));
-
-    setDays = new JTextField();
-    setDays.setMaximumSize(new Dimension(30, DConst.NPT11 * 2));
-
-    //JComboBox setDays initialisation
-    String [] amountDays = {"1","2","3","4","5","6","7","8","9","10"};
-    daysList = new JComboBox(amountDays);
-    daysList.setPreferredSize(new Dimension(50,DConst.NPT11 * 2));
-    daysList.setMaximumSize(new Dimension(50,DConst.NPT11 * 2));
-    daysList.setEditable(true);
-
-    //JComboBox setToolBar initialisation
-    setToolBar = new JComboBox(_strArray);
-    setToolBar.setPreferredSize(new Dimension(200,DConst.NPT11* 2));
-    setToolBar.setMaximumSize(new Dimension(200,DConst.NPT11 * 3));
-    setToolBar.setSelectedIndex(0);
-
-    add(setToolBar);
-
-    setToolBar.addActionListener(new ActionListener() {
-    public void actionPerformed(ActionEvent e) {
-      JComboBox cb = (JComboBox)e.getSource();
-      int  i = cb.getSelectedIndex();
-      switch (i){
-        case 0: addBarOne(); break;
-        case 1: addBarTwo(); break;
-      }// end switch
-
-    }
-   });
-    //System.out.println("Tool Bar Constructor");  // debug
-/*
-    // Create the first button.
-    String dir = System.getProperty("user.dir");
-
-        if (dir.endsWith("build")) {
-    dir = dir.substring(0, dir.lastIndexOf("build"));
-    } else{
-    System.out.println(System.getProperty("user.dir"));
-    }*/
-
-
-/*  setToolBar.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                  showText("Button 1 has been clicked");
-              }
-        });
-*/
+    //JComboBox toolBarSelector initialisation
+    toolBarSelector = new JComboBox(_toolBarNames);
+    toolBarSelector.setPreferredSize(new Dimension(200,DConst.NPT11* 2));
+    toolBarSelector.setMaximumSize(new Dimension(200,DConst.NPT11 * 3));
 
 
 
-   addDay.addActionListener(new ActionListener() {
-              public void actionPerformed(ActionEvent e) {
-                TTStructure tts = _dApplic.getDMediator().getCurrentDoc().getDM().getTTStructure();
-                tts.setNumberOfDays(1);
-                tts.modification();
-              }
-        });
+    add(toolBarSelector);
 
-  //     String command = e.getActionCommand();
-  /*  if (command.equals( BUT02 )) {  // cancel
-      //"Enseignants --> Bouton Annuler pressé\n"
-      dispose();
-    } else if (command.equals( BUT01 )) {  // OK
-   //   _ddv._jFrame._log.append("Enseignants --> Bouton OK pressé\n");
-       _currentInstr.setAvailability(_currentAvailbility);
-        _dm.incrementModification();
-      modified = false;
-      butApply.setEnabled(false);
-      dispose();
-    } else*/// if (command.equals("Apply" )) {  // apply
-    /*  "Enseignants --> Bouton Appliquer pressé\n");*/
-      //tts.s
-     // tts.modification();
-      //modified = false;
-      //butApply.setEnabled( false );
-    // if a button of the grid has been pressed
-    //}
-    // }
-    //
+    //JComboBox daySelector initialisation
+    String [] amountDays = {"1","2","3","4","5","6","7"};
+    daySelector = new JComboBox(amountDays);
+    daySelector.setPreferredSize(new Dimension(50,DConst.NPT11 * 2));
+    daySelector.setMaximumSize(new Dimension(50,DConst.NPT11 * 2));
+    daySelector.setEditable(true);
 
-/*
-   // fir BarOne
-   setToolBar.addActionListener(new ActionListener() {
-     public void actionPerformed(ActionEvent e) {
-       TTStructure tts = _dApplic.getDMediator().getCurrentDoc().getDM().getTTStructure();
-       String command = e.getActionCommand();
-  /*  if (command.equals( BUT02 )) {  // cancel
-      //"Enseignants --> Bouton Annuler pressé\n"
-      dispose();
-    } else if (command.equals( BUT01 )) {  // OK
-   //   _ddv._jFrame._log.append("Enseignants --> Bouton OK pressé\n");
-       _currentInstr.setAvailability(_currentAvailbility);
-        _dm.incrementModification();
-      modified = false;
-      butApply.setEnabled(false);
-      dispose();
-    } else*//* if (command.equals("Apply" )) {  // apply
-    /*  "Enseignants --> Bouton Appliquer pressé\n");*/
-      //tts.s
-     // tts.modification();
-      //modified = false;
-      //butApply.setEnabled( false );
-    // if a button of the grid has been pressed
-    //}
-    // }
-    //});*/
+    //JComboBox dayNameSelector initialisation
+    String [] dayNames = {"--","LU","MA","ME","JE","VE","SA","DI"};
+    dayNameSelector = new JComboBox(dayNames);
+    dayNameSelector.setPreferredSize(new Dimension(50,DConst.NPT11 * 2));
+    dayNameSelector.setMaximumSize(new Dimension(50,DConst.NPT11 * 2));
+    dayNameSelector.setEditable(true);
 
-/*
-    //Create the first button.
-    CmdButton select = new CmdButton( "AddText" );
-    add(select);
-    select.setCommand(new SelectCmd());
-    select.addActionListener(_dApplic);
-    addSeparator();
-*/
+    //JComboBox periodIndicator initialisation
+    String [] periodIndexes = {"1","2","3","4","5","6","7"};
+    periodSelector = new JComboBox(periodIndexes);
+    periodSelector.setPreferredSize(new Dimension(50,DConst.NPT11 * 2));
+    periodSelector.setMaximumSize(new Dimension(50,DConst.NPT11 * 2));
+    periodSelector.setEditable(true);
 
-/*
-    // Do the same for the second button
-    CmdButton resource = new CmdButton(_appFrame, dir + "/images/resource.gif", "Ressource" );
-    add(resource);
-    resource.setCommand(new ResourceCmd(_appFrame, _med, _stMgr));
-    resource.addActionListener(appFrame);
-    addSeparator();
+    //JComboBox periodTypeSelector initialisation
+    String [] periodTypes = {"Base Priorité","Normal","Null"};
+    periodTypeSelector = new JComboBox(periodTypes);
+    periodTypeSelector.setPreferredSize(new Dimension(100,DConst.NPT11 * 2));
+    periodTypeSelector.setMaximumSize(new Dimension(100,DConst.NPT11 * 2));
+    periodTypeSelector.setEditable(true);
 
-    // Do the same for the third button
-    CmdButton inductorRC = new CmdButton(_appFrame, dir +"/images/inductorRC.gif","InductorRC" );
-    add(inductorRC);
-    inductorRC.setCommand(new InductorRCCmd(_appFrame, _med, _stMgr));
-    inductorRC.addActionListener(_appFrame);
-    addSeparator();
+    toolBarSelector.setSelectedIndex(0);
+    addBarOne();// index 0
 
-    // Do the same for the fourth button
-    CmdButton consumer = new CmdButton(_appFrame, dir + "/images/consumer.gif","Consommateur" );
-    add(consumer);
-    consumer.setCommand(new ConsumerCmd(_appFrame, _med, _stMgr));
-    consumer.addActionListener(_appFrame);
-    addSeparator();
+    toolBarSelector.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        //JComboBox cb = (JComboBox)e.getSource();
+        //int  i = cb.getSelectedIndex();
+        int i = toolBarSelector.getSelectedIndex();
+        System.out.println("index i " + i);
+        switch (i){
+          case 0: addBarOne(); break;
+          case 1: addBarTwo(); break;
+        }// end switch
+      }//end actionPerformed
+    });//end addActionListener
 
-    // Do the same for the fifth button
-    CmdButton inductorCP = new CmdButton(_appFrame, dir + "/images/inductorCP.gif","InductorCP" );
-    add(inductorCP);
-    inductorCP.setCommand(new InductorCPCmd(_appFrame, _med, _stMgr));
-    inductorCP.addActionListener(_appFrame);
-    addSeparator();
 
-      CmdButton producer = new CmdButton(_appFrame, dir + "/images/producer.gif","Producer" );
-      add(producer);
-      producer.setCommand(new ProducerCmd(_appFrame,_med,_stMgr));
-      producer.addActionListener(_appFrame);
-      addSeparator();*/
+    //*** Actions for the elements of the bar one
 
-      //this.setFloatable(true);
-  }
+    setNumberOfDays.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    setNumberOfDays.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+
+    daySelector.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    dayNameSelector.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    stdDays.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    addDay.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    removeDay.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+
+    //*** Actions for the elements of the bar two
+
+    periodSelector.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+    periodTypeSelector.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+
+    sameLine.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+
+    sameColumn.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+
+      }//end actionPerformed
+    });//end addActionListener
+
+
+  }//end constructor
 
   //-------------------------------------------
   private void addBarOne() {
-    removeBarTwo();
-    add(lSetDays);
-    add(setDays);
-    add(lDaysList);
-    add(daysList);
-    add(lDayName);
-    add(dayName);
+    //removeBarTwo();
+    removeBar();
+    addSeparator();
+    add(lSetNumberOfDays);
+    add(setNumberOfDays);
+    addSeparator();
+    add(lDaySelector);
+    add(daySelector);
+    addSeparator();
+    add(lDayNameSelector);
+    add(dayNameSelector);
+    add(stdDays);
+    addSeparator();
     add(addDay);
+    addSeparator();
     add(removeDay);
-    add(doIt);
 
     repaint();
   }//end methode
 
   //-------------------------------------------
   private void removeBarOne() {
-    remove(lSetDays);
-    remove(setDays);
-    remove(lDaysList);
-    remove(daysList);
-    remove(lDayName);
-    remove(dayName);
+    remove(lSetNumberOfDays);
+    remove(setNumberOfDays);
+    remove(lDaySelector);
+    remove(daySelector);
+    remove(lDayNameSelector);
+    remove(dayNameSelector);
+    remove(stdDays);
     remove(addDay);
     remove(removeDay);
-    remove(doIt);
 
     repaint();
   }//end methode
 
   //-------------------------------------------
   private void addBarTwo() {
-    removeBarOne();
-    add(periodIndicator);
-    add(loPriority);
-    add(noPriority);
-    add(hiPriority);
-    add(doIt);
+    //removeBarOne();
+    removeBar();
+    addSeparator();
+    add(lPeriodIndicator);
+    add(periodSelector);
+    addSeparator();
+    add(lPeriodTypeSelector);
+    add(periodTypeSelector);
+    addSeparator();
     add(sameLine);
+    addSeparator();
     add(sameColumn);
 
     repaint();
-  }//end methode
+  }//end addBarTwo()
 
   //-------------------------------------------
   private void removeBarTwo() {
-    remove(periodIndicator);
-    remove(loPriority);
-    remove(noPriority);
-    remove(hiPriority);
+    remove(lPeriodIndicator);
+    remove(periodSelector);
+    remove(lPeriodTypeSelector);
+    remove(periodTypeSelector);
     remove(sameLine);
     remove(sameColumn);
-    remove(doIt);
 
     repaint();
-  }//end methode
+  }//end removeBarTwo()
 
+  private void removeBar() {
+    removeAll();
+    add(toolBarSelector);
 
-
-
-
-
-
-
-
-
-
+    repaint();
+  }//end removeBarTwo()
 
 
 } // end classe
