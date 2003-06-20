f822a280f2d2ecde4a463828c9b470e57496bbe1
ysyam <ysyam@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-06-20 15:56:03 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@722 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/ProtoTest.bat
+++ Dx/ProtoTest.bat
@@ -1,3 +1,3 @@
 rem coucou
-java -classpath "d:\Program Files\JBuilder7\extras\junit\junit.jar;D:\Developpements\DiamantExtreme\Dx\theLibrary\xmlLibrary.jar";D:\Developpements\Temp\Dx\Proto\classes\ junit.swingui.TestRunner DRunTest
+java -classpath "d:\Program Files\JBuilder7\extras\junit3.8.1\junit.jar;D:\Developpements\DiamantExtreme\Dx\theLibrary\xmlLibrary.jar";D:\Developpements\Temp\Dx\Proto\classes\ junit.swingui.TestRunner DRunTest
 
--- Dx/dTest/dInternal/dTimeTable/PeriodTest.java
+++ Dx/dTest/dInternal/dTimeTable/PeriodTest.java
@@ -18,6 +18,7 @@ import dInternal.dTimeTable.Period;
 
 import xml.InPut.readFile;
 import xml.InPut.ReadXMLElement;
+import xml.OutPut.*;
 
 import org.w3c.dom.Element;
 import org.w3c.dom.Document;
@@ -44,19 +45,21 @@ String path;
   public void test_readXMLtag(){
     readFile xmlFile;
     Element  item, ID;
+    Period period= new Period();
     try{
       xmlFile = new readFile();
-      System.out.println(path+"period.xml");//debug
-      Document  doc = xmlFile.getDocumentFile(path+"StandardTTC.xml");
+      //System.out.println(path+"period.xml");//debug
+      Document  doc = xmlFile.getDocumentFile(path+"period.xml");
       ReadXMLElement list= new ReadXMLElement();
       item= list.getRootElement(doc);
-      Period period= new Period();
       period.readXMLtag(item);
       //_setOfCycles.readXMLtag(root);
     }catch(Exception e){
       System.out.println(e);
     }
-    assertEquals("No test is required: : assertEquals", true, true);
+    assertEquals("test_readXMLtag : assertEquals 1(Hour):", period.getBeginHour()[0], 8);
+    assertEquals("test_readXMLtag : assertEquals 2(Minute):", period.getBeginHour()[1], 15);
+    assertEquals("test_readXMLtag : assertEquals 3(priotity):", period.getPriority(), 0);
   }
 
 
@@ -64,14 +67,64 @@ String path;
    * test that gives the end hour
    * */
   public void test_getEndHour(){
-    assertEquals("No test is required: : assertEquals", true, true);
+    readFile xmlFile;
+    Element  item, ID;
+    Period period= new Period();
+    try{
+      xmlFile = new readFile();
+      //System.out.println(path+"period.xml");//debug
+      Document  doc = xmlFile.getDocumentFile(path+"period.xml");
+      ReadXMLElement list= new ReadXMLElement();
+      item= list.getRootElement(doc);
+      period.readXMLtag(item);
+      //_setOfCycles.readXMLtag(root);
+    }catch(Exception e){
+      System.out.println(e);
+    }
+    assertEquals("test_getEndHour : assertEquals (endHour):", period.getEndHour(60)[0], 9);
+    assertEquals("test_getEndHour : assertEquals (endMinute):", period.getEndHour(60)[1], 15);
   }
 
   /**
    * test that gives the end hour
    * */
   public void test_writeXMLtag(){
-    assertEquals("No test is required: : assertEquals", true, true);
+    readFile xmlFile;
+    Element  item, ID;
+    Period period= new Period();
+    Period periodS= new Period();
+    try{
+      xmlFile = new readFile();
+      //System.out.println(path+"period.xml");//debug
+      Document  doc;// = xmlFile.getDocumentFile(path+"period.xml");
+      /*ReadXMLElement list= new ReadXMLElement();
+      item= list.getRootElement(doc);
+      period.readXMLtag(item);*/
+      //write xml file
+      period.setBeginHour(9,30);
+      period.setPriority(2);
+      BuildXMLElement wr= new BuildXMLElement();
+      doc=wr.getNewDocument();
+       Element ttPeriod= period.writeXMLtag(doc);
+      doc= wr.buildDOM(doc,ttPeriod);
+      writeFile.write(doc,path+"SavePeriod.xml");
+
+      // read xml file
+      doc = xmlFile.getDocumentFile(path+"SavePeriod.xml");
+      ReadXMLElement list= new ReadXMLElement();
+      item= list.getRootElement(doc);
+      periodS= new Period();
+      periodS.readXMLtag(item);
+      //Element ttStruc= _setOfCycles.writeXMLtag(doc);
+      // create document and write in the file
+
+      //_setOfCycles.readXMLtag(root);
+    }catch(Exception e){
+      System.out.println(e);
+    }
+    assertEquals("test_writeXMLtag : assertEquals (beginHour):", periodS.getBeginHour()[0], period.getBeginHour()[0]);
+    assertEquals("test_writeXMLtag : assertEquals (beginMinute):", periodS.getBeginHour()[1], period.getBeginHour()[1]);
+    assertEquals("test_writeXMLtag : assertEquals (priority):", periodS.getPriority(), period.getPriority());
   }
 
 
