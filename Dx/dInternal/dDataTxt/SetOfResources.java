1df4a831ec804f4202ab9765a00e40529d0e645e
ysyam <ysyam@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-08-20 13:54:58 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@853 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInternal/dDataTxt/SetOfResources.java
+++ Dx/dInternal/dDataTxt/SetOfResources.java
@@ -33,7 +33,8 @@ public class SetOfResources extends DXObject{
 
   /**
    * Constructor
-   * 0= activities, 1= students, 2= instructors, 3 = rooms
+   * 0= activities, 1= students, 2= instructors, 3 = rooms, 4= ttstructure
+   * 5= states
    * @param int resType  The resource type
    * */
   public SetOfResources( int resType) {
---
+++ Dx/dInternal/dDataTxt/SetOfStates.java
@@ -0,0 +1,36 @@
+package dInternal.dData;
+
+import java.awt.Color;
+
+public class SetOfStates extends SetOfResources{
+
+  String _error;
+  /***
+   * constructor
+   */
+
+  public SetOfStates() {
+    super(5);
+    _error="";
+  }
+
+  /**
+   *
+   */
+  public void buildSetOfStates(){
+    State state= new State(Color.BLACK,10);
+    addResource(new Resource("Nb de blocs:",state),0);
+    state= new State(Color.BLUE,2);
+    addResource(new Resource("Inst:",state),0);
+    state= new State(Color.RED,6);
+    addResource(new Resource("Stud:",state),0);
+  }
+
+  /**
+   *
+   * @return
+   */
+  public String getError() {
+    return _error;
+  }
+}
