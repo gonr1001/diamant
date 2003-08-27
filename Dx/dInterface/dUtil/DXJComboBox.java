a4bbdc73de64bdbe0f7cecaacb1d10abd3306be4
ysyam <ysyam@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-08-27 16:40:50 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@877 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dInterface/dUtil/DXJComboBox.java
@@ -0,0 +1,56 @@
+package dInterface.dUtil;
+
+import javax.swing.JComboBox;
+import java.util.Vector;
+import java.awt.event.ActionListener;
+
+public class DXJComboBox extends JComboBox{
+
+  ActionListener [] _actionList;
+  boolean _stateDisList=false;
+  /**
+   * Constructor
+   */
+  public DXJComboBox(){
+    super();
+  }
+
+  /**
+   * Constructor
+   */
+  public DXJComboBox(Vector items){
+    super(items);
+  }
+
+  /**
+   * Constructor
+   */
+  public DXJComboBox(Object[] items){
+    super(items);
+  }
+
+  /**
+   *
+   */
+  public void disableActionListener(){
+    _stateDisList= true;
+    _actionList = this.getActionListeners();
+    for (int i = 0; i <_actionList.length; i++) {
+      this.removeActionListener(_actionList[i]);
+    }
+  }
+
+  /**
+   *
+   */
+  public void enableActionListener(){
+    if(_stateDisList){
+      _stateDisList= false;
+      for (int i = 0; i <_actionList.length; i++) {
+        this.addActionListener(_actionList[i]);
+      }
+    }
+  }
+
+
+}
