aff6475ba326de4d60aad67b15f09fe6677dc9af
ysyam <ysyam@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-09-15 14:06:59 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@950 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dTest/dInternal/dOptimizationTest/StudentsConflictsMatrixTest.java
@@ -0,0 +1,89 @@
+package dTest.dInternal.dConditionsTest;
+
+/**
+ * <p>Title: Diamant</p>
+ * <p>Description:  timetable construction</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: UdeS</p>
+ * @author ysyam
+ * @version 1.0
+ */
+import junit.framework.*;
+
+import dInterface.DApplication;
+import dInterface.dUtil.DXTools;
+import dInternal.dConditionsTest.StudentsConflictsMatrix;
+import dInternal.dConditionsTest.EventAttach;
+import dInternal.dData.SetOfActivities;
+import dInternal.dData.SetOfStudents;
+import dInternal.DModel;
+import dInterface.DDocument;
+import java.util.Vector;
+import java.io.File;
+
+public class StudentsConflictsMatrixTest extends TestCase{
+
+StudentsConflictsMatrix _matrix;
+DModel _dm;
+//SetOfActivities _soa;
+//SetOfStudents _sos;
+//  public class ResourceTest extends TestCase {
+
+public StudentsConflictsMatrixTest(String name) {
+  super(name);
+  _dm= new DModel(new DApplication(),System.getProperty("user.dir")+ File.separator+"dataTest"+
+                    File.separator+"loadData.dia",0);
+  _dm.buildSetOfEvents();
+  //_dm.getConditionsTest().buildStudentsMatrix(_dm.getSetOfActivities(),_dm.getSetOfStudents());
+  _dm.getConditionsTest().buildAllConditions();
+  _dm.setStateBarComponent();
+  _matrix = _dm.getConditionsTest().getConflictsMatrix();
+}
+
+   public static Test suite() {
+    // the type safe way is in SimpleTest
+    // the dynamic way :
+    return new TestSuite(StudentsConflictsMatrixTest.class);
+   } // end suite
+
+   /**
+   *
+   */
+  public void test_SectionKeys(){
+    String key1= "ADM111.1.A";// key 1
+    String key2 = "GIN310.1.A";//key 136
+    int[] index= _matrix.getSectionsKeys(key1,key2);
+    assertEquals("test1_Matrix : assertEquals", 136, index[1]);
+    assertEquals("test1_Matrix : assertEquals", 1, index[0]);
+   }
+
+   /**
+    *
+    */
+   public void test1_Matrix(){
+     String key1= "ADM111.1.A";
+     String key2 = "GIN310.1.A";
+     assertEquals("test1_Matrix : assertEquals", 4, _matrix.getNumberOfCOnflicts(key1,key2));
+   }
+
+   /**
+    *
+    */
+   public void test2_Matrix(){
+     String key1= "ADM111.1.B";
+     String key2 = "GIN310.1.A";
+     assertEquals("test2_Matrix : assertEquals", 3, _matrix.getNumberOfCOnflicts(key1, key2));
+   }
+
+   /**
+    *
+    */
+   public void test3_Matrix(){
+     String key1= "ADM111.1.C";
+     String key2 = "GIN310.1.A";
+     assertEquals("test2_Matrix : assertEquals", 3, _matrix.getNumberOfCOnflicts(key1, key2));
+   }
+
+
+
+}
