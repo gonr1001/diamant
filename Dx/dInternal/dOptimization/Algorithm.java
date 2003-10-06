da53cd3622af0ccd6280fb8ee0c9c4c773ce2154
syay1801 <syay1801@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-10-06 20:53:37 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1033 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dInternal/dOptimization/Algorithm.java
@@ -0,0 +1,24 @@
+package dInternal.dAlgorithms;
+
+/**
+ * <p>Title: Diamant</p>
+ * <p>Description:  timetable construction</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: UdeS</p>
+ * @author ysyam
+ * @version 1.0
+ */
+
+import dInternal.dTimeTable.*;
+import dInternal.dConditionsTest.*;
+import java.util.Vector;
+
+public interface Algorithm {
+
+
+  /**
+   *
+   */
+  public void build(TTStructure tts, Vector vectorOfEvents);
+
+}
---
+++ Dx/dInternal/dOptimization/FirstAffectAlgorithm.java
@@ -0,0 +1,24 @@
+package dInternal.dAlgorithms;
+
+/**
+ * <p>Title: Proto</p>
+ * <p>Description:  timetable construction</p>
+ * <p>Copyright: Copyright (c) 2002</p>
+ * <p>Company: UdeS</p>
+ * @author unascribed
+ * @version 1.0
+ */
+
+import dInternal.dTimeTable.*;
+import dInternal.dConditionsTest.*;
+import java.util.Vector;
+
+public class FirstAffectAlgorithm implements Algorithm {
+
+  public FirstAffectAlgorithm() {
+  }
+
+  public void build(TTStructure tts, Vector vectorOfEvents){
+
+  }
+}
