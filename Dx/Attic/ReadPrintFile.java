bd6b513608044a75476e70ee3a90a8fd26492c8a
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2003-11-10 15:01:06 +0000

    no message
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1130 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/Attic/ReadPrintFile.java
+++ Dx/Attic/ReadPrintFile.java
@@ -1,37 +1,48 @@
 import java.io.*;
+
+import com.iLib.gIO.ByteInputFile;
 public class ReadPrintFile {
 
-    public static void main(String[] args) throws Exception{
+  public static void main(String[] args) throws Exception{
+    byte [] b;
+    ByteInputFile inputFile= new ByteInputFile (args[0]);
 
-    File inputFile = new File (args[0]);
-
-    if (!inputFile.exists()||!inputFile.canRead()){
-       System.out.println ("Impossible de lire "+inputFile);
-       return;
-    } //end if
-
+    b = inputFile.readFileAsBytes();
+    inputFile.close();
+ /*   if (!inputFile.available()||!inputFile.canRead()){
+      System.out.println ("Impossible de lire "+inputFile);
+      return;
+    } //end if*/
+    for (int i = 0 ; i < b.length; i ++ ) {
+      System.out.println("char: "+(char)b[i] +
+                         " int: "+ b[i] +
+                         " byte: " + (byte)b[i] +
+                         " short: "+(short)b[i] +
+                           " HEX:" + Integer.toHexString(b[i]).toUpperCase());
+    }
+/*
     try {
 
 
-        FileReader in = new FileReader(inputFile);
-        int c;
-        int i=0;
-        int limit=1000000;
+      FileReader in = new FileReader(inputFile);
+      int c;
+      int i=0;
+      int limit=1000000;
 
-        while (((c = in.read()) != -1)) //&&(i<limit))
-        {
-           System.out.println("char: "+(char)c +
-                              " int: "+ c +
-                              " byte: " + (byte)c +
-                              " short: "+(short)c +
-                              " HEX:" + Integer.toHexString(c).toUpperCase());
-            i++;
-        }
-        in.close();
+      while (((c = in.read()) != -1)) //&&(i<limit))
+      {
+        System.out.println("char: "+(char)c +
+                           " int: "+ c +
+                           " byte: " + (byte)c +
+                           " short: "+(short)c +
+                           " HEX:" + Integer.toHexString(c).toUpperCase());
+        i++;
+      }
+      in.close();
     }//end try
     catch(FileNotFoundException e){
-          System.out.println ("Le fichier n'existe plus.");
-    }//end catch
+      System.out.println ("Le fichier n'existe plus.");
+    }//end catch*/
 
- } // End of main
+  } // End of main
 }
