7e49281ef668499aedef7b55085b8c4332bbcc48
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2004-06-30 18:42:32 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1566 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/Attic/ReadPrintFile.java
+++ Dx/Attic/ReadPrintFile.java
@@ -14,13 +14,33 @@ public class ReadPrintFile {
     // System.out.println(isr.getEncoding());
     b = inputFile.readFileAsBytes();
     inputFile.close();
-
-    for (int i = 0 ; i < b.length; i ++ ) {
-      System.out.println("char: "+(char)b[i] +
-      " int: "+ b[i] +
-      " byte: " + (byte)b[i] +
-      " short: "+(short)b[i] +
-      " HEX:" + Integer.toHexString(b[i]).toUpperCase());
-    }
+	int i = 0;
+    while ( i < b.length) {
+     // System.out.println("char: "+(char)b[i] +
+     // " int: "+ b[i] +
+      //" byte: " + (byte)b[i] +
+      //" short: "+(short)b[i] +
+      //" HEX:" + Integer.toHexString(b[i]).toUpperCase());
+      String str1 = "";
+      String str2 = "";
+      if (i+16 < b.length) {
+		for(int j = 0 ; j < 16 ; j ++) {
+			   str1 += (byte) b[i + j ] + " ";
+			   if ( (byte)b[i + j ] < 126  && (byte)b[i + j] > 31 )
+				   str2 += (char) b[i + j ];
+			   else {
+			   	if ( (byte)b[i + j ] == 13  ||(byte)b[i + j] == 10 )
+			   		str2 += ".";
+			   	else 
+					str2 += (byte) b[i + j] + " ";
+			   }
+			 }
+			 System.out.println(str1 + "   " + str2);
+			 i += 16 ;
+      }     
+	  else {
+	  	;
+	  }
+    } // end while
   } // End of main
 }
