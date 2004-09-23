11d691ed7d34bdf8e53726670a94f9023d4868c0
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2004-09-23 14:05:18 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1602 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/ProtoTest.bat
+++
@@ -1,3 +0,0 @@
-rem coucou
-java -classpath "d:\Program Files\JBuilder7\extras\junit3.8.1\junit.jar;D:\Developpements\DiamantExtreme\Dx\theLibrary\theLibrary.jar;D:\Developpements\DiamantExtreme\Dx\theLibrary\xmlLibrary.jar";D:\Developpements\Temp\Dx\Proto\classes\ junit.swingui.TestRunner DRunTest
-
--- Dx/abbot.bat
+++
@@ -1,4 +0,0 @@
-set CP="D:\Developpements\abbot-0.12.2\lib\abbot.jar"
-set PATH="C:\Program Files\Java\j2sdk1.4.2_03\bin"
-java -version
-java -jar %CP% %1 %2 %3 %4 %5
