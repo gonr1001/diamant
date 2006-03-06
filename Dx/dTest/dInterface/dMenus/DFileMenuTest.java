ac98ca99a045716ede27f00f8db472d964d32e70
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-03-06 16:34:16 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1958 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dTest/dInterface/dMenus/DFileMenuTest.java
+++ Dx/dTest/dInterface/dMenus/DFileMenuTest.java
@@ -21,9 +21,9 @@
  */
 package dTest.dInterface.dMenus;
 
-import javax.swing.JFrame;
+
 import javax.swing.JMenu;
-import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -33,42 +33,56 @@ import dInterface.DApplication;
 import dInterface.dMenus.DFileMenu;
 import dInterface.dMenus.DxMenuBar;
 
-
 /**
  * @author : Ruben Gonzalez-Rubio
- *
- * Description: DFileMenuTest.java is a class used to: 
+ * 
+ * Description: DFileMenuTest.java is a class used to:
  * <p>
- *
+ * 
  */
-public class DFileMenuTest extends TestCase{
+public class DFileMenuTest extends TestCase {
 	DApplication _da;
+
 	DxMenuBar _dxmb;
+
 	public DFileMenuTest(String name) {
 		super(name);
 	}
-  
+
 	public static Test suite() {
 		// the type safe way is in SimpleTest
 		// the dynamic way :
 		return new TestSuite(DFileMenuTest.class);
 	} // end suite
-  
-	public void setUp(){
+
+	public void setUp() {
 		_da = new DApplication();
-		String [] args = {"-d"} ; 
+		String[] args = { "-d" };
 		_da.doIt(args);
 		_dxmb = _da.getDxMenuBar();
-		
+
 	}
-	
-	public void test_isDFileMenu(){				
-		JMenu dfm =(JMenu)_dxmb.getComponent(0);
-		assertEquals("test_isDFileMenu: assertEquals",true,dfm instanceof DFileMenu);
+
+	public void tesIsDFileMenu() {
+		JMenu dfm = (JMenu) _dxmb.getComponent(0);
+		assertEquals("testIsDFileMenu: assertEquals", true,
+				dfm instanceof DFileMenu);
 	}
-	
-	public void test_newTTable(){
-		DFileMenu dfm =(DFileMenu)_dxmb.getComponent(0);
-		assertEquals("test_addState: test_newTTable",true,dfm.getMenuComponent(0) instanceof JMenu);
+
+	public void testNewTTable() {
+		DFileMenu dfm = (DFileMenu) _dxmb.getComponent(0);
+		assertEquals("testNewTTable:", true, dfm
+				.getMenuComponent(0) instanceof JMenu);
+		JMenu jm = (JMenu) dfm.getMenuComponent(0);
+		assertEquals("testNewTTable1:", 0, jm.getText().compareToIgnoreCase(DConst.NEW_TT));
+	}
+
+	public void testNewTTableCycle() {
+		DFileMenu dfm = (DFileMenu) _dxmb.getComponent(0);
+		assertEquals("testNewTTableCycle: test_newTTable", true, dfm
+				.getMenuComponent(0) instanceof JMenuItem);
+		JMenuItem jmi = (JMenuItem) dfm.getComponent();
+		JMenuItem jmix = (JMenuItem) jmi.getComponent(0);
+		assertEquals("testNewTTableCycle1: test_newTTable", 0, jmix.getText().compareToIgnoreCase(DConst.NTT_CY));
 	}
 }
