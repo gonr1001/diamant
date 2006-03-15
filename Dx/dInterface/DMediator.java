e30de6efdc67b070db19670848ee831809ae1668
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-03-15 13:43:48 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@1962 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInterface/DMediator.java
+++ Dx/dInterface/DMediator.java
@@ -30,185 +30,195 @@ import dInterface.dTimeTable.SaveCmd;
 import eLib.exit.dialog.FatalProblemDlg;
 
 /**
- * Description: DMediator is a class used to 
+ * Description: DMediator is a class used as in the pattern
+ *     mediator to make a link between DApplication and the
+ *     current DDocument. There is only one currect document
+ *     the one which is at the top of all jInternalFrames
  * 
- *              <p>
- *              
- *
+ * <p>
+ * 
+ * 
  */
 
-public class DMediator extends Object{
+public class DMediator extends Object {
+
 	private DApplication _dApplication;
 
-    /**
-     * @associates DDocument 
-     */
 	private Vector<DDocument> _documents;
+
 	private boolean _cancel;
 
-//	private DMediator() {
-		//empty
-//	}// end Mediator
+	// -----------------------------
 
-	//-----------------------------
-	
 	public DMediator(DApplication dApplic) {
 		_dApplication = dApplic;
 		_documents = new Vector<DDocument>();
 		_cancel = false;
 	} // end Mediator
-	
+
 	public boolean getCancel() {
 		return _cancel;
 	}
-	
+
 	/**
 	 * for new tt and for open tt
-	 * @param ttname This string will be displayed as the title of the JIF
-	 * @param fileName is the full path file name containing the TTStructure
-	 * @param type is the type of timetable to be constructed
-	 * 		 possible types NO_TYPE = 0; CYCLE = 1; EXAM = 2; CYCLEANDEXAM = 3;
-	 *
+	 * 
+	 * @param ttname
+	 *            This string will be displayed as the title of the JIF
+	 * @param fileName
+	 *            is the full path file name containing the TTStructure
+	 * @param type
+	 *            is the type of timetable to be constructed possible types
+	 *            NO_TYPE = 0; CYCLE = 1; EXAM = 2; CYCLEANDEXAM = 3;
+	 * 
 	 */
 	public String addDoc(String ttName, String fileName, int type) {
 		DDocument currentDoc = new DDocument(this, ttName, fileName, type);
-		if(currentDoc.getError().length() == 0){
+		if (currentDoc.getError().length() == 0) {
 			_documents.addElement(currentDoc);
-			_dApplication.getToolBar().setToolBars(currentDoc.getCurrentDModel().getTTStructure());
+			_dApplication.getToolBar().setToolBars(
+					currentDoc.getCurrentDModel().getTTStructure());
 			_dApplication.hideToolBar();
-		}else{
-			new FatalProblemDlg(_dApplication.getJFrame(), currentDoc.getError());
+		} else {
+			new FatalProblemDlg(_dApplication.getJFrame(), currentDoc
+					.getError());
 			System.exit(1);
 		}
 		return currentDoc.getError();
-	} //end addDoc
-	
-	
-	
+	} // end addDoc
+
 	/**
 	 * for new ttStructure and for open ttStructure
-	 * @param fileName is the full path file name containing the TTStructure
-	 * @param type is the type of timetable to be constructed
-	 *
+	 * 
+	 * @param fileName
+	 *            is the full path file name containing the TTStructure
+	 * @param type
+	 *            is the type of timetable to be constructed
+	 * 
 	 */
 	public String addDoc(String fileName, int type) {
 		_dApplication.setCursorWait();
 		DDocument currentDoc = new DDocument(this, fileName, fileName, type);
 		_documents.addElement(currentDoc);
-		
-		if (currentDoc.getError().length() == 0){
-			//currentDoc.getDM().addAllListeners();
-			_dApplication.getToolBar().setToolBars(currentDoc.getCurrentDModel().getTTStructure());
-		}else{
-			new FatalProblemDlg(_dApplication.getJFrame(), currentDoc.getError());
+
+		if (currentDoc.getError().length() == 0) {
+			// currentDoc.getDM().addAllListeners();
+			_dApplication.getToolBar().setToolBars(
+					currentDoc.getCurrentDModel().getTTStructure());
+		} else {
+			new FatalProblemDlg(_dApplication.getJFrame(), currentDoc
+					.getError());
 			System.exit(1);
 		}
 		_dApplication.setCursorDefault();
 		return currentDoc.getError();
-	} //end addDoc
-	
-	public void removeCurrentDoc(){
+	} // end addDoc
+
+	public void removeCurrentDoc() {
 		_documents.remove(getCurrentDoc());
-		if (_documents.size()!=0) {
-			try{
+		if (_documents.size() != 0) {
+			try {
 				_documents.get(0).getJIF().setSelected(true);
-			} catch (PropertyVetoException e){
+			} catch (PropertyVetoException e) {
 				new FatalProblemDlg(_dApplication.getJFrame(), e.toString());
 				System.exit(1);
 			}
-		}else{//end if (_documents.size()!=0)
+		} else {// end if (_documents.size()!=0)
 			_dApplication.getToolBar().setEnabledToolbar(false);
 		}
-	} //end addDoc
-	//-------------------------------------------
-	
-	public String saveCurrentDoc(String str){
+	} // end addDoc
+
+	// -------------------------------------------
+
+	public String saveCurrentDoc(String str) {
 		getCurrentDoc().setDocumentName(str);
 		String error = "";
 		error = getCurrentDoc().getCurrentDModel().saveTimeTable(str);
 		return error;
 	}
-	
-	public void closeCurrentDoc(){
-		if (getCurrentDoc()!= null) {
+
+	public void closeCurrentDoc() {
+		if (getCurrentDoc() != null) {
 			if (getCurrentDoc().isModified()) {
 				_cancel = promptToSave();
-			} else {//end if
+			} else {// end if
 				DDocument aux = getCurrentDoc();
 				_documents.remove(getCurrentDoc());
 				aux.close();
-			} //end else
-			if(_documents.size()==0){
+			} // end else
+			if (_documents.size() == 0) {
 				_dApplication.hideToolBar();
-			}else{
-				_dApplication.getToolBar().setToolBars(getCurrentDoc().getCurrentDModel().getTTStructure());
+			} else {
+				_dApplication.getToolBar().setToolBars(
+						getCurrentDoc().getCurrentDModel().getTTStructure());
 			}
-		}//end if
-		
+		}// end if
+
 	}
-	//-------------------------------------------
+
+	// -------------------------------------------
 	public DDocument getCurrentDoc() {
 		DDocument currentDoc = null;
-		for (int i =0; i < _documents.size(); i++) {
-			currentDoc =  _documents.elementAt(i);
+		for (int i = 0; i < _documents.size(); i++) {
+			currentDoc = _documents.elementAt(i);
 			JInternalFrame currentFrame = currentDoc.getJIF();
 			if (currentFrame.isSelected()) {
 				return currentDoc;
 			} // end if
 		} // end for
-		if (_documents.size()!=0){
-			currentDoc =  _documents.elementAt(0);
-			try{
+		if (_documents.size() != 0) {
+			currentDoc = _documents.elementAt(0);
+			try {
 				currentDoc.getJIF().setIcon(false);
-			} catch (PropertyVetoException e){
+			} catch (PropertyVetoException e) {
 				new FatalProblemDlg(_dApplication.getJFrame(), e.toString());
 				System.exit(1);
 			}
 			return currentDoc;
 		}
 		return null;
-	} //end getCurrentDoc
-	
-	//-------------------------------------------
+	} // end getCurrentDoc
+
+	// -------------------------------------------
 	public JInternalFrame getCurrentFrame() {
 		DDocument currentDoc = getCurrentDoc();
 		if (currentDoc != null) {
 			return currentDoc.getJIF();
 		} // end if
 		return null;
-	} //end getCurrentFrame
+	} // end getCurrentFrame
+
 	/**
-	 * Prompts to save if document has changed.
-	 * This checks the document to see if it has changed since it was
-	 * loaded or last saved. If so, it prompts the user to save, not
-	 * save or cancel.
-	 *
-	 * @return false if the user cancels (presses the cancel button
-	 * or the dialog's close button).  Otherwise, it return true.
+	 * Prompts to save if document has changed. This checks the document to see
+	 * if it has changed since it was loaded or last saved. If so, it prompts
+	 * the user to save, not save or cancel.
+	 * 
+	 * @return false if the user cancels (presses the cancel button or the
+	 *         dialog's close button). Otherwise, it return true.
 	 */
 	private boolean promptToSave() {
-		int retval = JOptionPane.showConfirmDialog(_dApplication.getJFrame(), DConst.SAVE_PROMPT );
+		int retval = JOptionPane.showConfirmDialog(_dApplication.getJFrame(),
+				DConst.SAVE_PROMPT);
 		DDocument aux = getCurrentDoc();
-		switch ( retval ) {
+		switch (retval) {
 		case JOptionPane.YES_OPTION:
 			new SaveCmd().execute(_dApplication);
-		removeCurrentDoc();
-		aux.close();
-		return false;
-		
+			removeCurrentDoc();
+			aux.close();
+			return false;
+
 		case JOptionPane.NO_OPTION:
 			removeCurrentDoc();
-		aux.close();
-		return false;
-		
+			aux.close();
+			return false;
+
 		case JOptionPane.CANCEL_OPTION:
 		case JOptionPane.CLOSED_OPTION:
 			return true;
-		}//end switch
+		}// end switch
 		return true;// it does not matter
-	}//end promptToSave
-	
-	
+	}// end promptToSave
+
 	public DApplication getDApplication() {
 		return _dApplication;
 	}
