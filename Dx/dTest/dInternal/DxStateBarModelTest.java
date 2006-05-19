e30cee59cf59b040726b3d6ada51b4a83ead6eb7
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-05-19 16:00:10 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2054 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dTest/dInternal/DxStateBarModelTest.java
+++ Dx/dTest/dInternal/DxStateBarModelTest.java
@@ -36,19 +36,12 @@ import dInternal.DxStateBarModel;
  * 
  * Description: DxStateBarModelTest is a class used to:
  * <p>
- * TODO:insert comments
+ * Test DxStateBarModel
  * <p> 
  * 
  */
 public class DxStateBarModelTest extends TestCase {
 
-	private DDocument _doc5j;
-
-	private DDocument _doc7j;
-
-	private DModel _dm5j;
-
-	private DModel _dm7j;
 
 
 
@@ -62,21 +55,15 @@ public class DxStateBarModelTest extends TestCase {
 		return new TestSuite(DxStateBarModelTest.class);
 	} // end suite
 
-	public void setUp() {
+
+	public void testStateBarModel_5j() {
+		DDocument _doc5j;		
+		DModel _dm5j;
 		_doc5j = new DDocument();
 		String fileName = "." + File.separator + "dataTest" + File.separator
 				+ "loadData5j.dia";
 		int _type = 1;
 		_dm5j = new DModel(_doc5j, fileName, _type);
-
-//		_doc7j = new DDocument();
-//		String fileName1 = "." + File.separator + "dataTest" + File.separator
-//				+ "loadData7j.dia";
-//
-//		_dm7j = new DModel(_doc7j, fileName1, _type);
-	}
-
-	public void testStateBarModel_5j() {		
 		_dm5j.changeInDModel(new Object());
 		DxStateBarModel sbm = new DxStateBarModel(_dm5j);
 		sbm.update();
@@ -86,26 +73,34 @@ public class DxStateBarModelTest extends TestCase {
 		assertEquals("test3_StateBarModel_5j: assertEquals", 44, sbm.elementAt(3).getValue());
 		assertEquals("test4_StateBarModel_5j: assertEquals", 15, sbm.elementAt(4).getValue());
 		assertEquals("test5_StateBarModel_5j: assertEquals", 27, sbm.elementAt(5).getValue());
-		assertEquals("test6_StateBarModel_5j: assertEquals", 11, sbm.elementAt(6).getValue());
+		assertEquals("test6_StateBarModel_5j: assertEquals", 3, sbm.elementAt(6).getValue());
 		assertEquals("test7_StateBarModel_5j: assertEquals", 4, sbm.elementAt(7).getValue());
 		assertEquals("test8_StateBarModel_5j: assertEquals", 0, sbm.elementAt(8).getValue());
 		assertEquals("test9_StateBarModel_5j: assertEquals", 2, sbm.elementAt(9).getValue());
 		assertEquals("test10_StateBarModel_5j: assertEquals", 2, sbm.elementAt(10).getValue());		
 	}
 
-//	public void testStateBarModel_7j() {
-//		DxStateBarModel sbm = new DxStateBarModel(_dm7j);
-//		assertEquals("test0_StateBarModel_7j: assertEquals", -1, sbm.elementAt(0).getValue());
-//		assertEquals("test1_StateBarModel_7j: assertEquals", -1, sbm.elementAt(1).getValue());
-//		assertEquals("test2_StateBarModel_7j: assertEquals", -1, sbm.elementAt(2).getValue());
-//		assertEquals("test3_StateBarModel_7j: assertEquals", -1, sbm.elementAt(3).getValue());
-//		assertEquals("test4_StateBarModel_7j: assertEquals", -1, sbm.elementAt(4).getValue());
-//		assertEquals("test5_StateBarModel_7j: assertEquals", -1, sbm.elementAt(5).getValue());
-//		assertEquals("test6_StateBarModel_7j: assertEquals", -1, sbm.elementAt(6).getValue());
-//		assertEquals("test7_StateBarModel_7j: assertEquals", -1, sbm.elementAt(7).getValue());
-//		assertEquals("test8_StateBarModel_7j: assertEquals", -1, sbm.elementAt(8).getValue());
-//		assertEquals("test9_StateBarModel_7j: assertEquals", -1, sbm.elementAt(9).getValue());
-//		assertEquals("test10_StateBarModel_7j: assertEquals", -1, sbm.elementAt(10).getValue());
-//	}
+	public void testStateBarModel_7j() {
+		DDocument _doc7j;
+		DModel _dm7j;
+		_doc7j = new DDocument();
+		String fileName1 = "." + File.separator + "dataTest" + File.separator
+				+ "loadData7j.dia";
+		int _type = 1;
+		_dm7j = new DModel(_doc7j, fileName1, _type);
+		DxStateBarModel sbm = new DxStateBarModel(_dm7j);
+		sbm.update();
+		assertEquals("test0_StateBarModel_7j: assertEquals", -1, sbm.elementAt(0).getValue());
+		assertEquals("test1_StateBarModel_7j: assertEquals", 11, sbm.elementAt(1).getValue());
+		assertEquals("test2_StateBarModel_7j: assertEquals", 7, sbm.elementAt(2).getValue());
+		assertEquals("test3_StateBarModel_7j: assertEquals", 44, sbm.elementAt(3).getValue());
+		assertEquals("test4_StateBarModel_7j: assertEquals", 15, sbm.elementAt(4).getValue());
+		assertEquals("test5_StateBarModel_7j: assertEquals", 27, sbm.elementAt(5).getValue());
+		assertEquals("test6_StateBarModel_7j: assertEquals", 2, sbm.elementAt(6).getValue());
+		assertEquals("test7_StateBarModel_7j: assertEquals", 60, sbm.elementAt(7).getValue());
+		assertEquals("test8_StateBarModel_7j: assertEquals", 30, sbm.elementAt(8).getValue());
+		assertEquals("test9_StateBarModel_7j: assertEquals", 10, sbm.elementAt(9).getValue());
+		assertEquals("test10_StateBarModel_7j: assertEquals", 20, sbm.elementAt(10).getValue());
+	}
 
 }
