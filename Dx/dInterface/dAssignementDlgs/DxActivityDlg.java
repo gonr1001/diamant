16f9c765e5bf506bd875515c0a9a15f6af08ea01
caln1901 <caln1901@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-09-05 17:38:48 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2233 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ Dx/dInterface/dAssignementDlgs/DxActivityDlg.java
@@ -0,0 +1,250 @@
+package dInterface.dAssignementDlgs;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Iterator;
+import java.util.Vector;
+
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+
+import dConstants.DConst;
+import dDeveloper.DxFlags;
+import dInterface.DApplication;
+import dInterface.DlgIdentification;
+import dInterface.dAffectation.EditActivityDlg;
+import dInterface.dUtil.ButtonsPanel;
+import dInterface.dUtil.DxTools;
+import dInterface.dUtil.TwoButtonsPanel;
+import dInternal.DModel;
+import dInternal.dData.dActivities.DxActivity;
+import dInternal.dData.dActivities.DxSetOfActivities;
+import dInternal.dData.dActivities.SetOfActivities;
+
+public class DxActivityDlg extends JDialog implements ActionListener,
+		DlgIdentification {
+	private DApplication _dApplic;
+
+	private JDialog _jd;
+
+	/**
+	 * the lists containing the activities ID
+	 */
+	private JLabel _lVisible;
+
+	private JLabel _lNoVisible;
+
+	private JList _rightList;
+
+	private JList _leftList;
+
+	private ButtonsPanel _buttonsPanel;
+
+	private Object[] _currentActivities = new Object[0];
+
+	private DxSetOfActivities _dxsoaAct;
+
+	private String[] _arrowsNames = { DConst.TO_RIGHT, DConst.TO_LEFT };
+
+	/**
+	 * the vectors containing the activities ID
+	 */
+	private Vector<DxActivity> _dxavRight;
+
+	private Vector<DxActivity> _dxavLeft;
+
+	private DModel _dmodel;
+
+	/**
+	 * Dafault constructor
+	 * 
+	 * @param dApplic
+	 *            The application object (for extracting the JFrame)
+	 */
+
+	public DxActivityDlg(DApplication dApplic) {
+		super(dApplic.getJFrame(), DConst.ACT_LIST, true);
+		_dApplic = dApplic;
+		_jd = this; // to pass this dialog to the EditActivityDlg
+
+		if (DxFlags.newDoc) {
+			if (dApplic.getCurrentDxDoc() == null)
+				return;
+			_dmodel = dApplic.getCurrentDxDoc().getCurrentDModel();
+		} else {
+			if (dApplic.getCurrentDoc() == null)
+				return;
+			_dmodel = dApplic.getCurrentDModel();
+		}
+		_dxsoaAct = _dmodel.getDxSetOfActivities();
+
+		initialize();
+		int x = _dApplic.getJFrame().getX();
+		int y = _dApplic.getJFrame().getY();
+		this.setLocation(x + DConst.X_OFFSET, y + DConst.Y_OFFSET);
+		// this.setMinimumSize(new Dimension(300, 300));
+		// this.setPreferredSize(new Dimension(400, 400)); //the real
+		// this.setMaximumSize(new Dimension(500, 500)); // XXXX Pascal: lien
+		// inutile avec JDK 1.5
+		this.pack();
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	// XXXX Pascal: Fix Java 1.5->1.4
+	public Dimension getMinimumSize() {
+		return new Dimension(300, 300);
+	}
+
+	public Dimension getPreferredSize() {
+		return new Dimension(400, 400);
+	}
+
+	public Dimension getMaximumSize() {
+		return new Dimension(500, 500);
+	}
+
+	/**
+	 * Initialize the dialog
+	 */
+	protected void initialize() {
+		
+		_dxavRight = new Vector<DxActivity>();
+		_dxavLeft = new Vector<DxActivity>();
+		
+		DxActivity dxaCurrentAct;
+		Iterator itActivities = _dxsoaAct.iterator();
+		while(itActivities.hasNext()){
+			dxaCurrentAct = (DxActivity)itActivities.next();
+			if(dxaCurrentAct.getVisibility()){
+				_dxavLeft.add(dxaCurrentAct);
+			}else{
+				_dxavRight.add(dxaCurrentAct);
+			}
+			
+		}
+		
+		// right panel
+		_rightList = new JList(_dxavRight);
+		_rightList.addMouseListener(mouseListenerLists);
+
+		JPanel listPanel = DxTools.listPanel(_rightList);
+		_lNoVisible = new JLabel(_dxavRight.size() + " " + DConst.NOT_INCLUDED);
+		JPanel rightPanel = new JPanel(new BorderLayout());
+		listPanel.setMinimumSize(new Dimension(100, 100));
+		listPanel.setPreferredSize(new Dimension(100, 300));
+		listPanel.setMaximumSize(new Dimension(100, 400));
+		rightPanel.add(_lNoVisible, BorderLayout.NORTH);
+		rightPanel.add(listPanel, BorderLayout.SOUTH);
+		
+		// left panel
+		_leftList = new JList(_dxavLeft);
+		_leftList.addMouseListener(mouseListenerLists);
+		_lVisible = new JLabel(_dxavLeft.size() + " " + DConst.INCLUDED);
+		JPanel listPanelLeft = DxTools.listPanel(_leftList);
+		JPanel leftPanel = new JPanel();
+		listPanelLeft.setMinimumSize(new Dimension(100, 100));
+		listPanelLeft.setPreferredSize(new Dimension(100, 300));
+		listPanelLeft.setMaximumSize(new Dimension(100, 400));
+		leftPanel = new JPanel(new BorderLayout());
+		leftPanel.add(_lVisible, BorderLayout.NORTH);
+		leftPanel.add(listPanelLeft, BorderLayout.CENTER);
+		// arrows panel
+		JPanel arrowsPanel = DxTools.arrowsPanel(this, _arrowsNames, true);
+		// placing the panels and buttons into the _listsPanel
+		JPanel centerPanel = new JPanel();
+		centerPanel.add(leftPanel, BorderLayout.EAST);
+		centerPanel.add(arrowsPanel, BorderLayout.CENTER);
+		centerPanel.add(rightPanel, BorderLayout.WEST);
+		// _applyPanel
+		String[] a = { DConst.BUT_APPLY, DConst.BUT_CLOSE };
+		_buttonsPanel = new TwoButtonsPanel(this, a);
+		// Setting the button APPLY disable
+		_buttonsPanel.setFirstDisable();
+		// placing the elements into the JDialog
+		getContentPane().add(centerPanel, BorderLayout.CENTER);
+		getContentPane().add(_buttonsPanel, BorderLayout.SOUTH);
+	}// end initialize
+
+	/**
+	 * Define the mouse adapter and actions for the JList is
+	 */
+	private MouseListener mouseListenerLists = new MouseAdapter() {
+		public void mouseClicked(MouseEvent e) {
+			if (((JList) e.getSource()).getModel().getSize() == 0)
+				return;
+			if (e.getSource().equals(_leftList))
+				_rightList.clearSelection();
+			else
+				_leftList.clearSelection();
+			_currentActivities = ((JList) e.getSource()).getSelectedValues();
+			if (e.getClickCount() == 2) {
+				new EditActivityDlg(_jd, _dApplic,
+						(String) _currentActivities[0], false);
+			}// end if
+		}// end public void mouseClicked
+	};// end definition of MouseListener mouseListener = new MouseAdapter(){
+
+	/**
+	 * 
+	 * @param e
+	 *            an event
+	 */
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+		// if buttons CLOSE
+		if (command.equals(DConst.BUT_CLOSE))
+			dispose();
+		// if button APPLY
+		if (command.equals(DConst.BUT_APPLY)) {
+			setActivitesVisibility();
+			if (DxFlags.newDoc) {
+				_dmodel.changeInDModel(this.idDlgToString());
+			} else {
+				_dmodel.changeInDModelByActivity(this);
+			}
+			_buttonsPanel.setFirstDisable();
+		}
+		// if arrows
+		if (command.equals(_arrowsNames[0]) || command.equals(_arrowsNames[1])) {
+			if (command.equals(_arrowsNames[1])) {
+				DxTools.listTransfers(_rightList, _leftList, _rightVec,
+						_leftVec, 1);
+			} else {
+				DxTools.listTransfers(_leftList, _rightList, _leftVec,
+						_rightVec, 1);
+			}
+			_lNoVisible.setText(_dxavRight.size() + " " + DConst.NOT_INCLUDED);
+			_lVisible.setText(_dxavLeft.size() + " " + DConst.INCLUDED);
+			_buttonsPanel.setFirstEnable();
+		}// end if (command.equals(_arrowsNames[0]) ||
+			// command.equals(_arrowsNames[1]))
+	}// end method
+
+	/**
+	 * Sets the field "Visible" of the activities, according with their position
+	 * in the JLists. If an activity is in the _rightList, Visible = false.
+	 */
+	private void setActivitesVisibility() {
+		Iterator<DxActivity> itAct = _dxavRight.iterator();
+		while(itAct.hasNext()){
+			itAct.next().setVisibility(false);
+		}
+		
+		itAct = _dxavLeft.iterator();
+		while(itAct.hasNext()){
+			itAct.next().setVisibility(true);
+		}
+	}
+
+	public String idDlgToString() {
+		return this.getClass().toString();
+	}
+}
