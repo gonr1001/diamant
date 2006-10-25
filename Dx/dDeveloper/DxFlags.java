e4a9f4e799fc26bbc1238fee11dec9c0353d1d37
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-10-25 18:08:42 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2258 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dDeveloper/DxFlags.java
+++ Dx/dDeveloper/DxFlags.java
@@ -31,7 +31,7 @@ package dDeveloper;
 public class DxFlags {
 
 	 // public final static boolean newRooms = false;
-    public final static boolean newRooms = true;
+    public final static boolean newRooms = false;
 
 	public final static boolean newAlg = false;
 	// public final static boolean newAlg = false;
