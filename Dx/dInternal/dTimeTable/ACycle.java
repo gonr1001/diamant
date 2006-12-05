126ce31db68a2cbf7a909f90583dc1d3592bd2ea
abim1301 <abim1301@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2006-12-05 15:07:01 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2266 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInternal/dTimeTable/ACycle.java
+++ Dx/dInternal/dTimeTable/ACycle.java
@@ -24,7 +24,7 @@ public class ACycle {
 
 	
 	public ACycle() {
-	
+	/* */
 	}
 
 	public String getttcycleid() {
--- Dx/dInternal/dTimeTable/ASequence.java
+++ Dx/dInternal/dTimeTable/ASequence.java
@@ -25,7 +25,7 @@ public class ASequence {
 	private String _sequenceName;
 
 	public ASequence() {
-		
+		/* */
 	}
 
 	public int getttsequenceid() {
