71168fdf608ebde9ee61639206f8490841717ef7
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2007-01-01 17:41:31 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2278 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInterface/dUtil/ThreeButtonsPanel.java
+++ Dx/dInterface/dUtil/ThreeButtonsPanel.java
@@ -1,64 +1,64 @@
 /**
  *
- * Title: ThreeButtonsPanel $Revision: 1.3 $  $Date: 2004-06-21 15:38:18 $
- * Description: ThreeButtonsPanel is a class used to have three buttons in a panel:
- *               X, Apply and Close
- *
- *
- * Copyright (c) 2001 by rgr.
- * All rights reserved.
- *
- *
- * This software is the confidential and proprietary information
- * of rgr. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with rgr.
- *
- * @version $Revision: 1.3 $
- * @author  $Author: gonzrubi $
- * @since JDK1.3
- */
-package dInterface.dUtil;
-
-
-import java.awt.event.ActionListener;
-
-import javax.swing.JButton;
-
-
-
-public class ThreeButtonsPanel extends ButtonsPanel {
-
-  public ThreeButtonsPanel(ActionListener parentDialog, String [] buttonsNames) {
-    super();
-    JButton button = new JButton(buttonsNames[0]);
-    button.setActionCommand(buttonsNames[0]);
-    button.addActionListener(parentDialog);
-    this.add(button);
-    button = new JButton(buttonsNames[1]);
-    button.setActionCommand(buttonsNames[1]);
-    button.addActionListener(parentDialog);
-    this.add(button) ;
-    button = new JButton(buttonsNames[2]);
-    button.setActionCommand(buttonsNames[2]);
-    button.addActionListener(parentDialog);
-    this.add(button) ;
-  }
-
-  public void setFirstEnable() {
-    getComponent(0).setEnabled(true);
-  }
-
-  public void setFirstDisable() {
-    getComponent(0).setEnabled(false);
-  }
-
-  public void setMiddleEnable() {
-    getComponent(1).setEnabled(true);
-  }
-
-  public void setMiddleDisable() {
-    getComponent(1).setEnabled(false);
-  }
-}
+// * Title: ThreeButtonsPanel $Revision: 1.4 $  $Date: 2007-01-01 17:41:30 $
+// * Description: ThreeButtonsPanel is a class used to have three buttons in a panel:
+// *               X, Apply and Close
+// *
+// *
+// * Copyright (c) 2001 by rgr.
+// * All rights reserved.
+// *
+// *
+// * This software is the confidential and proprietary information
+// * of rgr. ("Confidential Information").  You
+// * shall not disclose such Confidential Information and shall use
+// * it only in accordance with the terms of the license agreement
+// * you entered into with rgr.
+// *
+// * @version $Revision: 1.4 $
+// * @author  $Author: gonzrubi $
+// * @since JDK1.3
+// */
+//package dInterface.dUtil;
+//
+//
+//import java.awt.event.ActionListener;
+//
+//import javax.swing.JButton;
+//
+//
+//
+//public class ThreeButtonsPanel extends ButtonsPanel {
+//
+//  public ThreeButtonsPanel(ActionListener parentDialog, String [] buttonsNames) {
+//    super();
+//    JButton button = new JButton(buttonsNames[0]);
+//    button.setActionCommand(buttonsNames[0]);
+//    button.addActionListener(parentDialog);
+//    this.add(button);
+//    button = new JButton(buttonsNames[1]);
+//    button.setActionCommand(buttonsNames[1]);
+//    button.addActionListener(parentDialog);
+//    this.add(button) ;
+//    button = new JButton(buttonsNames[2]);
+//    button.setActionCommand(buttonsNames[2]);
+//    button.addActionListener(parentDialog);
+//    this.add(button) ;
+//  }
+//
+//  public void setFirstEnable() {
+//    getComponent(0).setEnabled(true);
+//  }
+//
+//  public void setFirstDisable() {
+//    getComponent(0).setEnabled(false);
+//  }
+//
+//  public void setMiddleEnable() {
+//    getComponent(1).setEnabled(true);
+//  }
+//
+//  public void setMiddleDisable() {
+//    getComponent(1).setEnabled(false);
+//  }
+//}
