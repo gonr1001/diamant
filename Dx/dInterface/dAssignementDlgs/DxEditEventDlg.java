c07d9b28110f1d06720dba01b19be871895ac910
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2007-05-29 15:27:39 +0000

    *** empty log message ***
    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2362 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- Dx/dInterface/dAssignementDlgs/DxEditEventDlg.java
+++ Dx/dInterface/dAssignementDlgs/DxEditEventDlg.java
@@ -602,8 +602,8 @@ public class DxEditEventDlg extends JDialog implements ActionListener,
 		JComboBox cbRooms = new JComboBox();
 		System.out.println("room " + name);
 
-		if (name.equalsIgnoreCase("......")) { ///rgr
-			System.out.println("......" + name);
+		if (name.equalsIgnoreCase("......")||name.equalsIgnoreCase("------")) { ///rgr
+			System.out.println("......" + " ou " + "------" + name);
 			currentRoom = null;
 			currentCat = null;
 
