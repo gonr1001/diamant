271a979699497cef38b07e2b0711846c57a73d3e
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2009-02-09 19:45:42 +0000

    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2790 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

---
+++ src/dExceptions/DiaException.java
@@ -0,0 +1,38 @@
+/**
+ * Created on 2006-09-07
+ * 
+ * Title: DiaException.java 
+ * 
+ *
+ * Copyright (c) 2006 by rgr.
+ * All rights reserved.
+ *
+ *
+ * This software is the confidential and proprietary information
+ * of rgr. ("Confidential Information").  You
+ * shall not disclose such Confidential Information and shall use
+ * it only in accordance with the terms of the license agreement
+ * you entered into with rgr.
+ *
+ * @author HAROUNA Abdoul-Kader
+ * @since JDK1.3
+ */
+
+package dExceptions;
+
+public class DiaException extends Exception {
+
+	public DiaException() {
+		super();
+	}
+
+	public DiaException(String message) {
+		super(message);
+	}
+
+	public static String getCurrentMethod(Exception e) {
+		return e.getStackTrace()[0].getClassName() + "."
+				+ e.getStackTrace()[0].getMethodName() + "()!";
+	}
+
+}
