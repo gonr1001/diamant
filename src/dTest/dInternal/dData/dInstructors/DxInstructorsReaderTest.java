077fdf1b64ce0005cc094d1e55124b13e4d77313
gonzrubi <gonzrubi@bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df>
2009-05-25 21:52:42 +0000

    git-svn-id: svn+ssh://tictacserver.gel.usherbrooke.ca/Users/tictac/svnServer/developments/fullDiamant/diamant/trunk@2847 bc377d3f-2bd2-4e0d-9bbf-68e52cfa64df

--- src/dTest/dInternal/dData/dInstructors/DxInstructorsReaderTest.java
+++ src/dTest/dInternal/dData/dInstructors/DxInstructorsReaderTest.java
@@ -13,11 +13,10 @@ import dInternal.dData.dInstructors.DxInstructorsReader;
 import dInternal.dData.dInstructors.DxReadInstructorsdotDia;
 import dInternal.dData.dInstructors.DxSetOfInstructors;
 
-
 public class DxInstructorsReaderTest extends TestCase {
 
 	private final String _pathForFiles = "." + File.separator + "dataTest"
-	+ File.separator;
+			+ File.separator;
 
 	/**
 	 * 
@@ -30,12 +29,11 @@ public class DxInstructorsReaderTest extends TestCase {
 	} // end suite
 
 	/**
-	 * test_exceptionOnFirstLine, analysis should fail if an
-	 * invalid instructor format in first line
+	 * test_exceptionOnFirstLine, analysis should fail if an invalid instructor
+	 * format in first line
 	 */
 	public void test_exceptionOnFirstLine() {
-		String tokens = "    1k" + "\r\n" 
-			    + "ADM111" + "\r\n"
+		String tokens = "    1k" + "\r\n" + "ADM111" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
@@ -50,7 +48,9 @@ public class DxInstructorsReaderTest extends TestCase {
 			assertFalse("test_exceptionOnFirstLine: Should have failed before",
 					true);
 		} catch (DiaException e) {
-			assertEquals("test_exceptionOnFirstLine:",DConst.INVALID_NUMBER_OF_INSTRUCTORS+"For input string: \"1k\"", e.getMessage());			
+			assertEquals("test_exceptionOnFirstLine:",
+					DConst.INVALID_NUMBER_OF_INSTRUCTORS
+							+ "For input string: \"1k\"", e.getMessage());
 		}
 	}
 
@@ -58,21 +58,19 @@ public class DxInstructorsReaderTest extends TestCase {
 	 * test1_exceptionOnName, analysis fail if an invalid instructor name
 	 */
 	public void test1_exceptionOnName() {
-		String tokens = "    2" + "\r\n"  
-				+ " " + "\r\n"
+		String tokens = "    2" + "\r\n" + " " + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
-				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" 
-				+ "POLM" + "\r\n"
+				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" + "POLM" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n";
 		DxLoadData ld = new DxLoadData();
-	
+
 		DxInstructorsReader dxriTest = new DxReadInstructorsdotDia(ld
 				.insertHeader(tokens.getBytes()), 5, 14);
 		try {
@@ -82,24 +80,21 @@ public class DxInstructorsReaderTest extends TestCase {
 					true);
 		} catch (DiaException e) {
 			assertEquals("test1_1_getSetOfInstructors: assertEquals",
-					DConst.INVALID_INSTRUCTOR_NAME+"2", e
-							.getMessage());
+					DConst.INVALID_INSTRUCTOR_NAME + "2", e.getMessage());
 		}
 	}
 
 	/**
-	 * test_numberOfInstructors, analysis throws exception if
-	 * count and actual number of instructor in the file does not match
+	 * test_numberOfInstructors, analysis throws exception if count and actual
+	 * number of instructor in the file does not match
 	 */
 	public void test_numberOfInstructors() {
-		String tokens = "    3" + "\r\n" 
-		        + "JAC" + "\r\n"
+		String tokens = "    3" + "\r\n" + "JAC" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
-				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" 
-				+ "POLM" + "\r\n"
+				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" + "POLM" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
@@ -115,24 +110,22 @@ public class DxInstructorsReaderTest extends TestCase {
 					true);
 
 		} catch (DiaException e) {
-			assertEquals("test_numberOfInstructors: ",
-					 true, e.getMessage().contains(DConst.INVALID_NUMBER_OF_INSTRUCTORS));
+			assertEquals("test_numberOfInstructors: ", true, e.getMessage()
+					.contains(DConst.INVALID_NUMBER_OF_INSTRUCTORS));
 		}
 	}
 
 	/**
-	 * test_badAvailability, analysis throws a exception when
-	 * the availability format contains a 6
+	 * test_badAvailability, analysis throws a exception when the availability
+	 * format contains a 6
 	 */
 	public void test_badAvailability6() {
-		String tokens = "    2" + "\r\n" 
-		        + "John" + "\r\n"
+		String tokens = "    2" + "\r\n" + "John" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
-				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" 
-				+ "POLM" + "\r\n"
+				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" + "POLM" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1 6" + "\r\n"
@@ -148,23 +141,21 @@ public class DxInstructorsReaderTest extends TestCase {
 					true);
 		} catch (Exception e) {
 			assertEquals("test_badAvailability6: ",
-					DConst.INVALID_AVAILABILITY_AT+"11", e.getMessage());
+					DConst.INVALID_AVAILABILITY_AT + "11", e.getMessage());
 		}
 	}
 
 	/**
-	test_badAvailability, analysis throws a exception when
-	 * the availability format contains a 3
+	 * test_badAvailability, analysis throws a exception when the availability
+	 * format contains a 3
 	 */
 	public void test_badAvailability3() {
-		String tokens = "    2" + "\r\n" 
-		        + "JAC" + "\r\n"
+		String tokens = "    2" + "\r\n" + "JAC" + "\r\n"
 				+ "1 3 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
-				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" 
-				+ "POLM" + "\r\n"
+				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" + "POLM" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
@@ -180,7 +171,7 @@ public class DxInstructorsReaderTest extends TestCase {
 					true);
 		} catch (Exception e) {
 			assertEquals("test_badAvailability3:",
-					DConst.INVALID_AVAILABILITY_AT+"3", e.getMessage());
+					DConst.INVALID_AVAILABILITY_AT + "3", e.getMessage());
 		}
 	}
 
@@ -188,14 +179,12 @@ public class DxInstructorsReaderTest extends TestCase {
 	 * test5_getSetOfInstructors
 	 */
 	public void test_values() {
-		String tokens = "    2" + "\r\n" 
-				+ "JAC" + "\r\n"
+		String tokens = "    2" + "\r\n" + "JAC" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n"
-				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" 
-				+ "POLM" + "\r\n"
+				+ "1 5 5 5 5 5 5 5 5 5 5 5 5 5" + "\r\n" + "POLM" + "\r\n"
 				+ "1 1 5 1 5 1 5 1 5 1 5 1 5 1" + "\r\n"
 				+ "1 5 1 5 1 5 1 5 1 5 1 5 1 5" + "\r\n"
 				+ "1 5 5 5 5 5 5 5 5 5 5 1 1 1" + "\r\n"
@@ -206,45 +195,38 @@ public class DxInstructorsReaderTest extends TestCase {
 				.insertHeader(tokens.getBytes()), 5, 14);
 		try {
 			DxSetOfInstructors dxsoiTemp = dxriTest.readSetOfInstructors();
-			assertNotNull("test_values: assertNotNull",
-					dxsoiTemp);
-			assertNotNull("test_values: assertEquals",
-					dxsoiTemp.getResource("JAC"));
-			assertNotNull("test_values: assertEquals",
-					dxsoiTemp.getResource("POLM"));
+			assertNotNull("test_values: assertNotNull", dxsoiTemp);
+			assertNotNull("test_values: assertEquals", dxsoiTemp
+					.getResource("JAC"));
+			assertNotNull("test_values: assertEquals", dxsoiTemp
+					.getResource("POLM"));
 		} catch (Exception e) {
 			// Should not fail in tests
 			System.out.println("Exception in: test_values");
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * 
 	 *
 	 */
 	public void test_valuesOnDispoInst() {
-		DxSetOfInstructors dxsoi = null;
-		DxLoadData ld = new DxLoadData();
-		byte[] dataloaded = null;
-		try {
-			dataloaded = ld.filterBadChars(_pathForFiles + "testDispoInst.sig");
-		} catch (Exception e) {
-			// Should not fail in tests, but if file not there gives a failure
-			assertEquals("test_basicData: exception", "nullPointer", e.toString());
-			System.out.println("Exception in: test_valuesOnDispoInst");
-			e.printStackTrace();
-		}
-		
-		DataExchange de = ld.insertHeader(dataloaded);
 
-		DxInstructorsReader dxir = new DxReadInstructorsdotDia(de, 5, 14);
 		try {
-			dxsoi = dxir.readSetOfInstructors();
-			assertEquals("test6_1_getSetOfInstructors: assertEquals", 126, dxsoi
-					.size());
-			assertEquals("test6_2_getSetOfInstructors: assertEquals", true, dxsoi
-					.areVectorsSync());
+
+			DxLoadData ld = new DxLoadData();
+			byte[] dataloaded = ld.filterBadChars(_pathForFiles
+					+ "testDispoInst.sig");
+
+			DataExchange de = ld.insertHeader(dataloaded);
+
+			DxInstructorsReader dxir = new DxReadInstructorsdotDia(de, 5, 14);
+			DxSetOfInstructors dxsoi = dxir.readSetOfInstructors();
+			assertEquals("test6_1_getSetOfInstructors: assertEquals", 126,
+					dxsoi.size());
+			assertEquals("test6_2_getSetOfInstructors: assertEquals", true,
+					dxsoi.areVectorsSync());
 			assertNotNull("test6_3_getSetOfInstructors: assertEquals", dxsoi
 					.getResource("ABATZOGLOU, NICOLAS"));
 			assertNotNull("test6_4_getSetOfInstructors: assertEquals", dxsoi
@@ -252,26 +234,31 @@ public class DxInstructorsReaderTest extends TestCase {
 			assertNotNull("test6_5_getSetOfInstructors: assertEquals", dxsoi
 					.getResource("AMÉDIN, CELSE KAFUI"));
 
-			//assertEquals("test6_6_getSetOfInstructors: assertEquals", 5, dxsoi
-			//		.getInstructorAvailabilityByName("YAHIA, AMMAR").getPeriodAvailability(4,
-			//				13));
-//			assertEquals("test6_7_getSetOfInstructors: assertEquals", 1, dxsoi
-//					.getInstructorAvailabilityByKey(5).getPeriodAvailability(2, 7));
-//			dxsoi.removeInstructor(1);
-//			dxsoi.removeInstructor(126);
-//			assertEquals("test6_8_getSetOfInstructors: assertEquals", 124, dxsoi
-//					.size());
-//			assertEquals("test6_9_getSetOfInstructors: assertEquals", true, dxsoi
-//					.areVectorsSync());
-//			assertEquals("test6_10_getSetOfInstructors: assertEquals", -1, dxsoi
-//					.getInstructorKeyByName("YAHIA, AMMAR"));
+			// assertEquals("test6_6_getSetOfInstructors: assertEquals", 5,
+			// dxsoi
+			// .getInstructorAvailabilityByName("YAHIA, AMMAR").getPeriodAvailability(4,
+			// 13));
+			// assertEquals("test6_7_getSetOfInstructors: assertEquals", 1,
+			// dxsoi
+			// .getInstructorAvailabilityByKey(5).getPeriodAvailability(2, 7));
+			// dxsoi.removeInstructor(1);
+			// dxsoi.removeInstructor(126);
+			// assertEquals("test6_8_getSetOfInstructors: assertEquals", 124,
+			// dxsoi
+			// .size());
+			// assertEquals("test6_9_getSetOfInstructors: assertEquals", true,
+			// dxsoi
+			// .areVectorsSync());
+			// assertEquals("test6_10_getSetOfInstructors: assertEquals", -1,
+			// dxsoi
+			// .getInstructorKeyByName("YAHIA, AMMAR"));
 		} catch (Exception e) {
 			// Should not fail in tests, but if file not there gives a failure
-			assertEquals("test_basicData: exception", "nullPointer", e.toString());
+			assertEquals("test_basicData: exception", "nullPointer", e
+					.toString());
 			System.out.println("Exception in: test_valuesOnDispoInst");
 			e.printStackTrace();
 		}
-
 
 	}
 
